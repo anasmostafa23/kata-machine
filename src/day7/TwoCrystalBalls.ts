0ec7ac48bc8de3a7b0c076a45b7cf995cb72c0ba
anasmostafa23 <anasmostafa23@yahoo.com>
2024-02-27 11:47:26 +0500

    balls

--- src/day7/TwoCrystalBalls.ts
+++ src/day7/TwoCrystalBalls.ts
@@ -1,3 +1,23 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
 
+    let jump = Math.floor(Math.sqrt(breaks.length)) ;
+
+    let i = 0 ;
+    for(;i < breaks.length ; i += jump ) {
+
+        if (breaks[i])
+            break ; 
+    }
+
+    let j = i - jump ;
+
+
+    for (;j < i ; j++) {
+        if (breaks[j])
+            return j ; 
+    }
+
+    return -1 ; 
+
+
 }
